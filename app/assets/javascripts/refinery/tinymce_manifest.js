6cb1969c7a8d9295b34f90e128d0efbe165c5d06
guillaume sebrier <guillaume.sebrier@gmail.com>
2025-10-15 13:50:54 +0200

    NEO-830 Adapt to Air Indemnité config

--- app/assets/javascripts/refinery/tinymce_manifest.js
+++ app/assets/javascripts/refinery/tinymce_manifest.js
@@ -12,13 +12,21 @@
  *= require tinymce/themes/silver/theme.min.js
  *= require tinymce/themes/mobile/theme.min.js
  *= require tinymce/plugins/link/plugin.min.js
- *= require tinymce/plugins/image/plugin.min.js
- *= require tinymce/plugins/fullscreen/plugin.min.js
- *= require tinymce/plugins/lists/plugin.min.js
  *= require tinymce/plugins/code/plugin.min.js
- *= require tinymce/plugins/stylebuttons/plugin.min.js
+ *= require tinymce/plugins/emoticons/plugin.min.js
+ *= require tinymce/plugins/help/plugin.min.js
+ *= require tinymce/plugins/image/plugin.min.js
+ *= require tinymce/plugins/imagetools/plugin.min.js
+ *= require tinymce/plugins/importcss/plugin.min.js
+ *= require tinymce/plugins/lists/plugin.min.js
+ *= require tinymce/plugins/nonbreaking/plugin.min.js
+ *= require tinymce/plugins/paste/plugin.min.js
  *= require tinymce/plugins/refineryimage/plugin.min.js
  *= require tinymce/plugins/refinerylink/plugin.min.js
+ *= require tinymce/plugins/stylebuttons/plugin.min.js
+ *= require tinymce/plugins/table/plugin.min.js
+ *= require tinymce/plugins/template/plugin.min.js
+ *= require tinymce/plugins/fullscreen/plugin.min.js
  *= require refinery/boot_tinymce
  *= require_self
-*/
+ */
